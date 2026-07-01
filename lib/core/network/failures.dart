c3e71834831be041327c05628cc64cd459dcb5d6
Mostafa_Amer <amer2052001@gmail.com>
2026-07-01 17:23:39 +0300

    fix packages versions (retrofit & retrofit_generator) to stable version

--- lib/core/network/failures.dart
+++ lib/core/network/failures.dart
@@ -41,6 +41,8 @@ class ServerFailure extends Failure {
         );
       case DioExceptionType.badResponse:
         return ServerFailure.fromResponse(dioException.response);
+      case DioExceptionType.transformTimeout:
+        throw UnimplementedError();
     }
   }
 
